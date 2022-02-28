e0271e78d96c4a5af06fd193a9a609692ab11258
Aakash Kumar <aakashskumar198765@gmail.com>
2022-02-28 21:01:16 +0530

    backup

--- android/app/src/main/java/com/regula/documentreader/MainApplication.java
+++ android/app/src/main/java/com/regula/documentreader/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.bitgo.randombytes.RandomBytesPackage;
 import com.reactnativecommunity.androidprogressbar.RNCProgressBarPackage;
 import com.reactApilibrary.DlApiPackage;
 import com.mg.app.PickerPackage;
